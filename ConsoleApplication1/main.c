6ff7392771527b39719c08ef872689a260e641bc
Wesley <wesley_costa@outlook.com>
2016-10-04 16:02:20 -0300

    asdasdad

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+	int valor;
+}ELEMENTO;
+
+
+typedef struct
+{
+	ELEMENTO elemento;
+	ELEMENTO *proximo;
+	ELEMENTO *anterior;
+}ELEMENTO_NO;
+
+typedef struct lista
+{
+	ELEMENTO *topo;
+	int totalElementos;
+}LISTA;
+
+LISTA* CriarLista()
+{
+	LISTA *pLista = (LISTA *)malloc(sizeof(LISTA));
+	pLista->topo = NULL;
+	pLista->totalElementos = 0;
+
+	return pLista;
+}
 
 int main()
 {
