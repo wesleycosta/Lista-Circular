fbb0c7b5cff4c9efc0c489f667b209127fb339ca
Wesley <wesley_costa@outlook.com>
2016-10-04 17:14:14 -0300

    commit

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -6,7 +6,6 @@ typedef struct
 	int valor;
 }ELEMENTO;
 
-
 typedef struct
 {
 	ELEMENTO elemento;
@@ -16,28 +15,30 @@ typedef struct
 
 typedef struct lista
 {
-	ELEMENTO_NO *topo;
-	ELEMENTO_NO *incio;
+	ELEMENTO_NO *inicio;
+	ELEMENTO_NO *fim;
 	int totalElementos;
 }LISTA;
 
 LISTA* CriarLista()
 {
 	LISTA *pLista = (LISTA *)malloc(sizeof(LISTA));
-	pLista->topo = NULL;
+	pLista->inicio = 0;
+	pLista->fim = NULL;
 	pLista->totalElementos = 0;
+
 	return pLista;
 }
 
 int ListaVazia(LISTA *pLista)
 {
-	return (!pLista->topo);
+	return !pLista->inicio;
 }
 
 // VERIFICA SE DOIS ELEMENTOS SÃO IGUAIS
 int Igual(ELEMENTO A, ELEMENTO B)
 {
-	return(A.valor == B.valor);
+	return A.valor == B.valor;
 }
 
 // BUSCA UM ELEMENTO DA LISTA, CASA NÃO EXISTA RETORNA NULL
@@ -46,9 +47,9 @@ ELEMENTO_NO *BuscarElemento(LISTA *pLista, ELEMENTO elemento)
 	ELEMENTO_NO *pIndex = NULL;
 	int index = 0;
 
-	for (pIndex = pLista->topo; index < pLista->totalElementos; pIndex = pIndex->proximo)
-		if (Igual(pIndex->elemento, elemento))
-			return pIndex;
+	for (pIndex = pLista->fim; index < pLista->totalElementos; pIndex = pIndex->proximo)
+	if (Igual(pIndex->elemento, elemento))
+		return pIndex;
 
 	return NULL;
 }
@@ -70,23 +71,24 @@ int Inserir(LISTA *pLista, ELEMENTO elemento)
 		{
 			novoElemento->proximo = novoElemento; // O PROXIMO DO NOVO ELEMENTO APONTA PARA O INICIO DA FILA  (NOVO ELEMENTO)
 			novoElemento->anterior = novoElemento; // O ANTERIRO DO NOVO ELEMENTO APONTA PARA O FINAL DA FILA (NOVO ELEMENTO)
-			pLista->incio = novoElemento; // INICIO DA FILA RECEBE O NOVO ELEMENTO
+			pLista->inicio = novoElemento; // INICIO DA FILA RECEBE O NOVO ELEMENTO
 		}
 		else
 		{
-			novoElemento->anterior = pLista->topo; // O ANTERIOR DO NOVO ELEMENTO APONTA PARA O ULTIMO ELEMENTO DA FILA
-			novoElemento->proximo = pLista->incio; // O PROXIMO  DO NOVO ELEMENTO APONTA PARA O INICIO DA FILA
+			novoElemento->anterior = pLista->fim; // O ANTERIOR DO NOVO ELEMENTO APONTA PARA O ULTIMO ELEMENTO DA FILA
+			novoElemento->proximo = pLista->inicio; // O PROXIMO  DO NOVO ELEMENTO APONTA PARA O INICIO DA FILA
 
-			pLista->topo->proximo = novoElemento;   // O PROXIMO DO ULTIMO ELEMNTO APONTA PARA O NOVO ELEMENTO
-			pLista->incio->anterior = novoElemento; // O ANTERIOR DO INCIO APONTA PARA O FINAL DA LISTA
+			pLista->fim->proximo = novoElemento;   // O PROXIMO DO ULTIMO ELEMNTO APONTA PARA O NOVO ELEMENTO
+			pLista->inicio->anterior = novoElemento; // O ANTERIOR DO inicio APONTA PARA O FINAL DA LISTA
 		}
 
-		pLista->topo = novoElemento; // TOPO RECEBE O NOVO ELEMENTO
+		pLista->fim = novoElemento; // fim RECEBE O NOVO ELEMENTO
 		return pLista->totalElementos++;
 	}
 }
 
-int Remover(LISTA*pLista, ELEMENTO elemento)
+// REMOVER UM ELEMENTO DA LISTA
+int Remover(LISTA *pLista, ELEMENTO elemento)
 {
 	ELEMENTO_NO *elementoEncontrado = BuscarElemento(pLista, elemento);
 
@@ -95,17 +97,66 @@ int Remover(LISTA*pLista, ELEMENTO elemento)
 	else
 	{
 		ELEMENTO_NO * elementoAntes = elementoEncontrado->anterior;
+		ELEMENTO_NO * elementoDepois = elementoEncontrado->proximo;
+
+		elementoAntes->proximo = elementoEncontrado->proximo;
+		elementoDepois->anterior = elementoAntes;
+
+		if (elementoEncontrado == pLista->fim)
+		{
+			pLista->fim = elementoAntes;
+			pLista->inicio->anterior = elementoAntes;
+		}
+		else if (elementoEncontrado == pLista->inicio)
+		{
+			pLista->inicio = elementoDepois;
+			pLista->fim->proximo = elementoDepois;
+		}
+
+		free(elementoEncontrado);
+		return pLista->totalElementos--;
+	}
+}
+
+// ORDERNA A LISTA
+void Ordernar(LISTA *pLista)
+{
+	ELEMENTO_NO *pElAtual;
+	ELEMENTO_NO *pElProximo;
+
+	int i = 0, j = 0;
+
+	for (pElAtual = pLista->inicio; i < pLista->totalElementos; pElAtual = pElAtual->proximo)
+	{
+		for (pElProximo = pElAtual->proximo; j < pLista->totalElementos - 1; pElProximo = pElProximo->proximo)
+		{
+			if (pElAtual->elemento.valor < pElProximo->elemento.valor)
+			{
+				ELEMENTO x = pElAtual->elemento;
+				pElAtual->elemento = pElProximo->elemento;
+				pElProximo->elemento = x;
+			}
+			j++;
+		}
+
+		j = 0; i++;
 	}
 }
 
 // IMPRIME TODOS OS ELEMENTOS DA LISTA
 void Imprimir(LISTA *pLista)
 {
-	ELEMENTO_NO *pIndex = pLista->topo;
+	ELEMENTO_NO *pIndex = pLista->fim;
 	int index = 0;
 
-	for (pIndex = pLista->topo; index < pLista->totalElementos; pIndex = pIndex->anterior)
+	for (pIndex = pLista->inicio; index < pLista->totalElementos; pIndex = pIndex->proximo)
 		printf("[%d] - %d \n", index++, pIndex->elemento.valor);
+}
+
+void Pause()
+{
+	printf("\n\nTecle <ENTER> para continuar...");
+	getchar();
 }
 
 int main()
@@ -114,7 +165,7 @@ int main()
 	ELEMENTO elemento;
 	int i = 0;
 
-	for (i = 1; i <= 5; i++)
+	for (i = 10; i > 0; i--)
 	{
 		elemento.valor = i;
 		Inserir(pLista, elemento);
@@ -122,6 +173,19 @@ int main()
 
 	Imprimir(pLista);
 
-	getchar();
+	printf("\n\n");
+
+	elemento.valor = 1;
+	Remover(pLista, elemento);
+	elemento.valor = 3;
+	Remover(pLista, elemento);
+	elemento.valor = 4;
+	Remover(pLista, elemento);
+
+	Ordernar(pLista);
+
+	Imprimir(pLista);
+
+	Pause();
 	return 0;
 }
